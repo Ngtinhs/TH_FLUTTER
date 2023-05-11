57cbbf2ef686e526ff84f3ba0d58236f7e3564ff
ngtinh <nguyenvanductinh77@gmail.com>
2023-05-11 10:37:55 +0700

    Update Hiển thị sản phẩm vào cate

--- thuchanh/lib/homepage/components/product_in_category.dart
+++ thuchanh/lib/homepage/components/product_in_category.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:demo/model/products.dart';
+import 'package:demo/model/utilities.dart';
+import '../../../detail/productpage.dart';
+
 class Food {
   final String title;
 
@@ -24,7 +28,7 @@ class FoodListPage extends StatefulWidget {
 }
 
 class _FoodListPageState extends State<FoodListPage> {
-  late Future<List<Food>> foodsFuture;
+  late Future<List<Products>> foodsFuture;
 
   @override
   void initState() {
@@ -32,13 +36,29 @@ class _FoodListPageState extends State<FoodListPage> {
     foodsFuture = fetchFoods();
   }
 
-  Future<List<Food>> fetchFoods() async {
+  Future<List<Products>> fetchFoods() async {
     final response = await http.get(Uri.parse(
         'http://192.168.15.109:8000/api/categories/${widget.categoryId}'));
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body);
       final List<dynamic> foodData = jsonData['foods'];
-      return foodData.map((food) => Food.fromJson(food)).toList();
+      List<Products> result = [];
+      try {
+        for (var element in foodData) {
+          var data = Products(
+            description: element['description'],
+            title: element['title'],
+            image: element['image'],
+            price: double.parse(element['price']),
+            quantity: element['quantity'],
+            id: element['_id'],
+          );
+          result.add(data);
+        }
+      } catch (e) {
+        print(e.toString());
+      }
+      return result;
     } else {
       throw Exception('Failed to load foods');
     }
@@ -50,15 +70,55 @@ class _FoodListPageState extends State<FoodListPage> {
       appBar: AppBar(
         title: Text('Food List'),
       ),
-      body: FutureBuilder<List<Food>>(
+      body: FutureBuilder<List<Products>>(
         future: foodsFuture,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(snapshot.data![index].title),
+                return GestureDetector(
+                  onTap: () {
+                    Utilities.result.add(snapshot.data![index]);
+                    Navigator.pushNamed(
+                      context,
+                      ProductPage.routeName,
+                      arguments: ProductDetailsArguments(
+                        product: snapshot.data![index],
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            snapshot.data![index].image,
+                            fit: BoxFit.fill,
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            snapshot.data![index].title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            snapshot.data![index].price.toString(),
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 );
               },
             );
